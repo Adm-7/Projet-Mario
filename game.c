b460632bde90b465cc8eecf6e6bbb38eabf500a2
Adm-7 <admrayene129@gmail.com>
2025-04-02 11:05:09 +0200

    commit

--- game.c
+++ game.c
@@ -4,22 +4,53 @@
 #include "event.h"
 
 int jouer(SDL_Renderer* renderer) {
-    
-	//charger image et personnage. 
+    // Initialisation des variables
+    int continuer = 1; // À utiliser pour savoir si on continue la boucle du jeu ou si on arrête
+    SDL_Event event;  // Déclaration de l'événement
 
-    int continuer = 1; //a utiliser pour savoir si on continue la boucle du jeu ou si on arrête. 
-    SDL_Event events;
+    // Configuration de la couleur de fond (blanc)
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+   
+    while(continuer) { // Coeur du jeu ici, les actions seront répétées pour faire le déplacement des différentes images, etc.
+        SDL_RenderClear(renderer);
 
-    while(continuer){ //coeur du jeu ici, les actions seront repété pour faire le déplacement des différentes images, ...
-        
-        //gérer les différentes actions. 
-        
-        
+        // Gérer les différentes actions
+        while (SDL_PollEvent(&event)) {  // Boucle qui gère les événements
+            switch (event.type) {
+                case SDL_QUIT:  // Si l'utilisateur ferme la fenêtre
+                    continuer = 0;
+                    break;
+
+                case SDL_KEYDOWN:  // Si une touche est pressée
+                    switch(event.key.keysym.sym) {
+                        case SDLK_ESCAPE:  // Si la touche échappe est pressée
+                            continuer = 0;
+                            break;
+
+                        case SDLK_1:  // Si la touche '1' est pressée
+                            // Ici tu peux ajouter le code pour afficher la map Mario
+                            // Exemple : afficherMap(renderer, map, sprites);
+                            break;
+
+                        default:
+                            break;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        // À ce stade, tu devrais dessiner et mettre à jour les objets du jeu
+        // Afficher la scène et mettre à jour l'écran
+        SDL_RenderPresent(renderer);
+
     }
-    
-	//a vous de compléter, au fur et à mesure, les deux fonctions en dessous pour bien faire le nettoyage. 
-    //LibererMap(map, sprites);
-    //freePersonnage(mario, goomba, nbGoomba);
 
-    return continuer;
+    // Libération des ressources une fois le jeu terminé
+    // LibererMap(map, sprites);   // Décommenter et compléter cette ligne selon ton code
+    // freePersonnage(mario, goomba, nbGoomba);  // Libérer la mémoire allouée pour les personnages
+
+    return continuer;  // Retourne la valeur de "continuer" pour quitter ou non
 }
